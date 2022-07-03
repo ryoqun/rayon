c3e66c13d5cff30bbee841a4c4753d79611da977
Ryo Onodera <ryoqun@gmail.com>
2022-07-03 22:46:43 +0900

    save

--- rayon-core/src/lib.rs
+++ rayon-core/src/lib.rs
@@ -315,7 +315,7 @@ impl<C: CustomCollector> ThreadPoolBuilder<C> {
     ///     Ok(())
     /// }
     /// ```
-    pub fn build_scoped<W, F, R, C: CustomCollector>(self, wrapper: W, with_pool: F) -> Result<R, ThreadPoolBuildError>
+    pub fn build_scoped<W, F, R>(self, wrapper: W, with_pool: F) -> Result<R, ThreadPoolBuildError>
     where
         W: Fn(ThreadBuilder::<C>) + Sync, // expected to call `run()`
         F: FnOnce(&ThreadPool::<C>) -> R,
