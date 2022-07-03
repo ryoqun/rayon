eb64f63bc8d213089e4f420405dfa014bb6def39
Ryo Onodera <ryoqun@gmail.com>
2022-07-03 13:51:00 +0900

    save

--- rayon-core/src/registry.rs
+++ rayon-core/src/registry.rs
@@ -590,7 +590,7 @@ impl ThreadInfo {
 /// ////////////////////////////////////////////////////////////////////////
 /// WorkerThread identifiers
 
-pub(super) struct WorkerThread<C: CustomCollector> {
+pub(super) struct WorkerThread<C: CustomCollector = DefaultCollector> { // TODO
     /// the "worker" half of our local deque
     worker: Worker<JobRef, DefaultCollector>,
 
