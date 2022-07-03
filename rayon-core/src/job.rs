32f6cb12ff1958349f11271c1b11af9d98c5a534
Ryo Onodera <ryoqun@gmail.com>
2022-07-03 13:03:08 +0900

    save

--- rayon-core/src/job.rs
+++ rayon-core/src/job.rs
@@ -1,6 +1,7 @@
 use crate::latch::Latch;
 use crate::unwind;
-use crossbeam_deque::{Injector, Steal, DefaultCollector};
+use crossbeam_deque::{Injector, Steal};
+use crossbeam_epoch::DefaultCollector;
 use std::any::Any;
 use std::cell::UnsafeCell;
 use std::mem;
