f8eb41462ac60f40ce5236f39ee94876aee14105
Ryo Onodera <ryoqun@gmail.com>
2022-07-03 16:06:56 +0900

    save

--- rayon-core/src/lib.rs
+++ rayon-core/src/lib.rs
@@ -267,13 +267,13 @@ where
     /// changed. Therefore, if you call `build_global` a second time, it
     /// will return an error. An `Ok` result indicates that this
     /// is the first initialization of the thread pool.
-    pub fn build_global(self) -> Result<(), ThreadPoolBuildError> {
         /*
+    pub fn build_global(self) -> Result<(), ThreadPoolBuildError> {
         let registry = registry::init_global_registry(self)?;
         registry.wait_until_primed();
         Ok(())
-        */
     }
+        */
 }
 
 impl ThreadPoolBuilder {
