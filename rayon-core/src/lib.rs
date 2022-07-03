f1e005cd15f85315d3063473537bec03f5903f4b
Ryo Onodera <ryoqun@gmail.com>
2022-07-03 23:21:13 +0900

    save

--- rayon-core/src/lib.rs
+++ rayon-core/src/lib.rs
@@ -211,8 +211,6 @@ type ExitHandler = dyn Fn(usize) + Send + Sync;
 // NB: We can't `#[derive(Default)]` because `S` is left ambiguous.
 impl<S: ThreadSpawn<C>, C: CustomCollector> Default for ThreadPoolBuilder<S, C> {
     fn default() -> Self {
-        panic!();
-        /*
         ThreadPoolBuilder::<S, C> {
             num_threads: 0,
             panic_handler: None,
@@ -224,7 +222,6 @@ impl<S: ThreadSpawn<C>, C: CustomCollector> Default for ThreadPoolBuilder<S, C> 
             breadth_first: false,
             phantom: PhantomData::default(),
         }
-        */
     }
 }
 
