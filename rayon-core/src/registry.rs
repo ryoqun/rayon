929e72dce5479e3cea1b34867265d7e716860424
Ryo Onodera <ryoqun@gmail.com>
2022-07-04 12:38:46 +0900

    save

--- rayon-core/src/registry.rs
+++ rayon-core/src/registry.rs
@@ -805,9 +805,7 @@ impl<C: CustomCollector> WorkerThread<C> {
                 dbg!(("steal1", &s));
             }
             if s == "rayon_core::registry::TypeErasedCustomCollector" {
-                dbg!(("type erase", (
-                            &std::any::type_name::<Self>()));
-                self.dyn_collector.name());
+                dbg!(("type erase", &std::any::type_name::<Self>(), self.dyn_collector.name());
                 panic!("type erase detected!")
             }
             let job = (start..num_threads)
