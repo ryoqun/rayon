8211a84312dcb2858e625ed5d9c1cf354d89c495
Ryo Onodera <ryoqun@gmail.com>
2022-07-04 13:31:27 +0900

    save

--- rayon-core/src/registry.rs
+++ rayon-core/src/registry.rs
@@ -803,10 +803,10 @@ impl<C: CustomCollector> WorkerThread<C> {
             let start = self.rng.next_usize(num_threads);
             let s = std::any::type_name::<C>().to_string();
             if s != "crossbeam_rayon_many_threads::MyCustomCollector" {
-                dbg!(("steal1", &s));
+                //dbg!(("steal1", &s));
             }
             if s == "rayon_core::registry::TypeErasedCustomCollector" {
-                dbg!(("type erase", &std::any::type_name::<Self>(), self.dyn_collector.name()));
+                //dbg!(("type erase", &std::any::type_name::<Self>(), self.dyn_collector.name()));
                 //panic!("type erase detected!")
             }
             let job = (start..num_threads)
