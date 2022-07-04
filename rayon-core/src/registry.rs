b62ca4490380a7d395f9b702bd7db8d60a1349ec
Ryo Onodera <ryoqun@gmail.com>
2022-07-04 10:58:59 +0900

    save

--- rayon-core/src/registry.rs
+++ rayon-core/src/registry.rs
@@ -794,6 +794,7 @@ impl<C: CustomCollector> WorkerThread<C> {
         loop {
             let mut retry = false;
             let start = self.rng.next_usize(num_threads);
+            dbg!(("steal", std::any::type_name::<C>()));
             let job = (start..num_threads)
                 .chain(0..start)
                 .filter(move |&i| i != self.index)
