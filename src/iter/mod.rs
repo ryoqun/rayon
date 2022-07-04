d01c0ef035b2b18f795e638d1f443ba7dcddb3a8
Ryo Onodera <ryoqun@gmail.com>
2022-07-04 21:37:44 +0900

    save

--- src/iter/mod.rs
+++ src/iter/mod.rs
@@ -589,11 +589,9 @@ pub trait ParallelIterator: Sized + Send {
         Map::new(self, map_op)
     }
 
-    /*
-    fn install() -> Install<Self> {
-        Install::new(self)
+    fn install() -> Self {
+        Self
     }
-    */
 
     /// Applies `map_op` to the given `init` value with each item of this
     /// iterator, producing a new iterator with the results.
