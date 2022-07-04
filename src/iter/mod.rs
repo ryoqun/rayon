bc32292dd3aa5dcf92553a0e8e46d53db2ec79da
Ryo Onodera <ryoqun@gmail.com>
2022-07-04 23:44:07 +0900

    save

--- src/iter/mod.rs
+++ src/iter/mod.rs
@@ -352,7 +352,7 @@ pub trait ParallelIterator: Sized + Send {
     ///
     /// [`for_each`]: #method.for_each
     type Item: Send;
-    type Base;
+    type Base: ParallelIterator = [];
 
     /// Executes `OP` on each item produced by the iterator, in parallel.
     ///
