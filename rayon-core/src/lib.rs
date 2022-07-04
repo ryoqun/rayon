f772db098c46e59adde6940203a50855f8b530e2
Ryo Onodera <ryoqun@gmail.com>
2022-07-04 10:02:22 +0900

    save

--- rayon-core/src/lib.rs
+++ rayon-core/src/lib.rs
@@ -234,7 +234,7 @@ impl<S: ThreadSpawn<C>, C: CustomCollector> ThreadPoolBuilder<S, C> {
 
 /// Note: the `S: ThreadSpawn` constraint is an internal implementation detail for the
 /// default spawn and those set by [`spawn_handler`](#method.spawn_handler).
-impl<S: ThreadSpawn<C>, C: CustomCollector> ThreadPoolBuilder<S, C>
+impl<S: 'static + ThreadSpawn<C>, C: CustomCollector> ThreadPoolBuilder<S, C>
 {
     /// Creates a new `ThreadPool` initialized using this configuration.
     pub fn build(self) -> Result<ThreadPool<C>, ThreadPoolBuildError> {
