46d98252dbe5c4e5f2774203b2ad94be408796a2
Ryo Onodera <ryoqun@gmail.com>
2022-07-05 00:02:34 +0900

    save

--- src/iter/enumerate.rs
+++ src/iter/enumerate.rs
@@ -93,13 +93,11 @@ pub trait WithInstallType<CC: CustomCollector> {
     }
 }
 
-/*
 impl<I, CC: CustomCollector> WithInstallType<CC> for InstallType<I, CC>
 where
     I: ParallelIterator,
 {
 }
-*/
 
 impl<I: ParallelIterator<Base = G>, CC: CustomCollector, G: WithInstallType<CC>> WithInstallType<CC> for I {
 }
