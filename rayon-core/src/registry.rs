9165c6a43cb95bf782350d9999b53421b13b5950
Ryo Onodera <ryoqun@gmail.com>
2022-07-04 10:01:39 +0900

    save

--- rayon-core/src/registry.rs
+++ rayon-core/src/registry.rs
@@ -86,7 +86,7 @@ pub trait ThreadSpawn<C: CustomCollector>: Default {
 #[derive(Debug, Default)]
 pub struct DefaultSpawn;
 
-impl<C: CustomCollector> ThreadSpawn<C> for DefaultSpawn {
+impl<C: 'static + CustomCollector> ThreadSpawn<C> for DefaultSpawn {
     private_impl! {}
 
     fn spawn(&mut self, thread: ThreadBuilder<C>) -> io::Result<()> {
