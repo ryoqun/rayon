c1669a771732c2c6c2e75aff328cc79fac046811
Ryo Onodera <ryoqun@gmail.com>
2022-07-04 21:46:10 +0900

    save

--- src/iter/mod.rs
+++ src/iter/mod.rs
@@ -590,7 +590,7 @@ pub trait ParallelIterator: Sized + Send {
     }
 
     /// jjaajj
-    fn install<C>(self, a: PhantomData<C>) -> Self {
+    fn install<C>(self, a: std::marker::PhantomData<C>) -> Self {
         panic!("install(): {}", std::any::type_name::<C>().to_string());
         self
     }
