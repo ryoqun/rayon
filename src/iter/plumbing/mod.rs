d25ca02e79059929eadc9f82b384eb30091cc9d7
Ryo Onodera <ryoqun@gmail.com>
2022-07-05 21:56:51 +0900

    save

--- src/iter/plumbing/mod.rs
+++ src/iter/plumbing/mod.rs
@@ -23,6 +23,7 @@ pub trait ProducerCallback<T> {
     ///
     /// [Output]: https://doc.rust-lang.org/std/ops/trait.FnOnce.html#associatedtype.Output
     type Output;
+    type InstalledCollector = u8;
 
     /// Invokes the callback with the given producer as argument. The
     /// key point of this trait is that this method is generic over
@@ -399,7 +400,7 @@ where
         C: Consumer<I>,
     {
         type Output = C::Result;
-        //type InstalledCollector = II::Base;
+        type InstalledCollector = II::Base;
 
         fn callback<P>(self, producer: P) -> C::Result
         where
