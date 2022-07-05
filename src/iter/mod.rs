1e2113e44db59e663c25aaba7cd4ed5660e9f2bb
Ryo Onodera <ryoqun@gmail.com>
2022-07-05 20:44:02 +0900

    save

--- src/iter/mod.rs
+++ src/iter/mod.rs
@@ -2982,7 +2982,7 @@ where
     /// [`par_iter.fold`]: trait.ParallelIterator.html#method.fold
     /// [`par_iter.fold_with`]: trait.ParallelIterator.html#method.fold_with
     /// [`par_iter.for_each`]: trait.ParallelIterator.html#method.for_each
-    fn from_par_iter<I>(par_iter: I) -> Self
+    fn from_par_iter<I, CC = crossbeam_deque::DefaultCollector>(par_iter: I) -> Self
     where
         I: IntoParallelIterator<Item = T>;
 }
