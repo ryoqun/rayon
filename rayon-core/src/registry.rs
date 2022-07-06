8242c3026cdf91ea25e0856318a8c9f94334e825
Ryo Onodera <ryoqun@gmail.com>
2022-07-06 09:42:57 +0900

    save

--- rayon-core/src/registry.rs
+++ rayon-core/src/registry.rs
@@ -812,7 +812,7 @@ impl<C: CustomCollector> WorkerThread<C> {
                 //panic!("type erase detected!")
             }
             */
-            dbg!("right before steal: {:?}", (std::any::type_name::<C>()));
+            dbg!(std::thread::current::name(), "right before steal: {:?}", (std::any::type_name::<C>()));
             let job = (start..num_threads)
                 .chain(0..start)
                 .filter(move |&i| i != self.index)
